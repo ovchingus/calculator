247b9834c0257020c82f65a0f063c6c163dae716
Vladimir Ovechkin <ovchingus@gmail.com>
2019-09-25 18:41:48 +0300

    Fix quit sign

--- main.py
+++ main.py
@@ -6,19 +6,21 @@ inp = input_output.InputOutput()
 prs = parser.Parser()
 calc = calculator.Calculator('base')
 
-while inp.get_input != 'q':
-    try:
-      command = inp.get_input()
-    except (IOError, KeyboardInterrupt):
-      print '\nReturning to main menu...'
+while True:
+  try:
+    command = inp.get_input()
+  except (IOError, KeyboardInterrupt):
+    print '\nReturning to main menu...'
 
-    arr = prs.parse(command)
+  arr = prs.parse(command)
 
-    if arr[1] == '+':
-      print (calc.plus(float(arr[0]), float(arr[2])))
-    if arr[1] == '-':
-      print (calc.minus(float(arr[0]), float(arr[2])))
-    if arr[1] == '*':
-      print (calc.multiply(float(arr[0]), float(arr[2])))
-    if arr[1] == '/':
-      print (calc.divide(float(arr[0]), float(arr[2])))
+  if arr[0] == 'q\n':
+    break
+  if arr[1] == '+':
+    print (calc.plus(float(arr[0]), float(arr[2])))
+  if arr[1] == '-':
+    print (calc.minus(float(arr[0]), float(arr[2])))
+  if arr[1] == '*':
+    print (calc.multiply(float(arr[0]), float(arr[2])))
+  if arr[1] == '/':
+    print (calc.divide(float(arr[0]), float(arr[2])))
