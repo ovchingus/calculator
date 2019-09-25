5a147d42d8c395af4bc375afa0859028c3a491db
Vladimir Ovechkin <ovchingus@gmail.com>
2019-09-25 16:20:17 +0300

    Migrated to class

--- calculator.py
+++ calculator.py
@@ -1,11 +1,19 @@
-def plus(a, b):
+class Calculator:
+
+  def __init__(self, name):
+    self.name = name
+
+  def change_name(self, new_name):
+    self.name = new_name
+
+  def plus(self, a, b):
     return a + b
 
-def minus(a,b):
+  def minus(self, a, b):
     return a - b
 
-def multiply(a,b):
+  def multiply(self, a, b):
     return a * b
 
-def divide(a,b):
+  def divide(self, a, b):
     return a / b
