e43d74189ff7fcb32ae24a3a3f61ff3711187b5a
Vladimir Ovechkin <ovchingus@gmail.com>
2019-09-28 17:01:58 +0300

    --wip-- [skip ci]

--- test_parser.py
+++ test_parser.py
@@ -10,5 +10,8 @@ class TestInputOutput(unittest.TestCase):
   def test_parse1(self):
     self.assertEqual(prs.parseBase('12 * 4'), ['12', '*', '4'])
 
+  def test_parse_trig(self):
+    self.assertEqual(prs.parseTrig('cos(10)'), ['cos', '10'])
+
 if __name__ == '__main__':
   unittest.main()
